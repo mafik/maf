d4221c3ba33f88cbd4110db50a46bfdecec4a25c
Marek Rogalski <mafikpl@gmail.com>
2023-07-14 16:06:59 +0200

    Added some formatting functions from Gatekeeper

--- src/format.cc
+++ src/format.cc
@@ -17,4 +17,52 @@ std::string f(const char *fmt, ...) {
   return std::string(buf);
 }
 
+std::string IndentString(std::string in, int spaces) {
+  std::string out(spaces, ' ');
+  for (char c : in) {
+    out += c;
+    if (c == '\n') {
+      for (int i = 0; i < spaces; ++i) {
+        out += ' ';
+      }
+    }
+  }
+  return out;
+}
+
+std::string Slugify(std::string in) {
+  std::string out;
+  bool unk = false;
+  for (char c : in) {
+    if (c >= 'A' && c <= 'Z') {
+      if (unk) {
+        if (!out.empty()) {
+          out += '-';
+        }
+        unk = false;
+      }
+      out += c - 'A' + 'a';
+    } else if (c >= 'a' && c <= 'z') {
+      if (unk) {
+        if (!out.empty()) {
+          out += '-';
+        }
+        unk = false;
+      }
+      out += c;
+    } else if (c >= '0' && c <= '9') {
+      if (unk) {
+        if (!out.empty()) {
+          out += '-';
+        }
+        unk = false;
+      }
+      out += c;
+    } else {
+      unk = true;
+    }
+  }
+  return out;
+}
+
 } // namespace maf
--- src/format.hh
+++ src/format.hh
@@ -7,4 +7,26 @@ namespace maf {
 // TODO: replace this with std::format when it's available
 Str f(const char *fmt, ...);
 
+// Prefix each line with `spaces` spaces.
+std::string IndentString(std::string in, int spaces = 2);
+
+std::string Slugify(std::string in);
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wformat-security"
+constexpr void constexpr_sprintf(std::string &out, const char *format,
+                                 auto... args) {
+  int n = snprintf(nullptr, 0, format, args...) + 1;
+  char buf[n];
+  snprintf(buf, n, format, args...);
+  out += buf;
+}
+#pragma clang diagnostic pop
+
+template <typename T> std::string dump_struct(const T &t) {
+  std::string s;
+  __builtin_dump_struct(&t, constexpr_sprintf, s);
+  return s;
+}
+
 } // namespace maf
